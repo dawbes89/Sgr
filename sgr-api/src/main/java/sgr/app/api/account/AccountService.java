bcbf82d404e334bd03d17f94e1ac96defdf009fc
leonzio <leonzio@localhost>
2015-11-27 18:15:21 +0100

    #10 review

--- sgr-api/src/main/java/sgr/app/api/account/AccountService.java
+++ sgr-api/src/main/java/sgr/app/api/account/AccountService.java
@@ -5,10 +5,16 @@ import java.util.List;
 /**
  * @author dawbes
  */
+//REVIEW serwis pod taką nazwą obecnie potrzebny nie jest chyba, że z metodą fingAccountByLogin(String login)
+// jeśli znajdzie taki account to go zwroci jesli nie to wyrzuci bład
+// A do logowania i wylogowywania zrób inny serwis - też jeszcze do obgadania sprawa encji użytkowników
+// mam pewnien pomysł aby nie zmieniać tego co jest obecnie
 public interface AccountService
 {
+   //REVIEW nie wiem czy listowanie kont nam się przyda, ale to jeszcze do obgadania
    public List<Account> search(AccountQuery query);
 
+   //REVIEW do zmiany tak jak mówiłem - przeniesienie do innego serwisu
    public boolean checkLogin(String userName, String password);
 
 }
