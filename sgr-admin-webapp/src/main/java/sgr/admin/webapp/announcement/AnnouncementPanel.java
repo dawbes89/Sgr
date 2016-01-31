21f517a722b8317886034cfc09906ab259472711
dawbes89 <dawbes89@localhost>
2016-01-31 20:49:57 +0100

    #31 Stworzenie panelu do dodawania aktualno?ci przez adminstratora

---
+++ sgr-admin-webapp/src/main/java/sgr/admin/webapp/announcement/AnnouncementPanel.java
@@ -0,0 +1,60 @@
+package sgr.admin.webapp.announcement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import sgr.app.api.announcement.Announcement;
+import sgr.app.api.announcement.AnnouncementService;
+import sgr.app.frontend.AbstractPanel;
+import sgr.app.frontend.EditablePanel;
+
+/**
+ * 
+ * @author dawbes
+ *
+ */
+@Controller
+public class AnnouncementPanel extends AbstractPanel<Announcement> implements EditablePanel<Announcement>
+{
+
+   private static final long serialVersionUID = 6851863797450712604L;
+   
+   @Autowired
+   private AnnouncementService announcementService;
+
+   @Override
+   public void init()
+   {
+      entity = new Announcement();
+      entities = announcementService.search();
+   }
+   
+   @Override
+   public void create()
+   {
+      announcementService.create(entity);
+      init();
+   }
+
+   @Override
+   public void update(Announcement object)
+   {
+     announcementService.update(entity);
+     init();
+   }
+
+   @Override
+   public void remove(Long id)
+   {
+      announcementService.delete(entity);
+      init();
+   }
+
+
+   @Override
+   public void onLoad()
+   {
+      init();   
+   }
+
+}
