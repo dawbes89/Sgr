2871e0f449a4733010bb10d83a3586a4d502934a
dawbes89 <dawbes89@gmail.com>
2016-02-06 21:53:24 +0100

    #31 poprawa panelu w sgr-webapp

--- sgr-app-webapp/src/main/java/sgr/app/webapp/announcement/AnnouncementPanel.java
+++ sgr-app-webapp/src/main/java/sgr/app/webapp/announcement/AnnouncementPanel.java
@@ -1,69 +1,37 @@
 package sgr.app.webapp.announcement;
 
 import java.io.Serializable;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import sgr.app.api.announcement.Announcement;
 import sgr.app.api.announcement.AnnouncementService;
+import sgr.app.frontend.panels.AbstractPanel;
 
 /**
  * @author dawbes
  */
 @Controller
-// REVIEW ten panel powinien rozszerzaæ AbstractPanel i mieæ w xhtml onLoad
-public class AnnouncementPanel implements Serializable
+public class AnnouncementPanel extends AbstractPanel<Announcement> implements Serializable
 {
 
    private static final long serialVersionUID = 3526253311725754381L;
 
    @Autowired
    private AnnouncementService announcementService;
-   // REVIEW praktycznie ca³a treœc idzie do wywalenia i najlepiej
-   // zaimplementowaæ abstractPanel
 
-   public AnnouncementService getAnnouncementService()
-   {
-      return announcementService;
-   }
-
-   public void setAnnouncementService(AnnouncementService announcementService)
-   {
-      this.announcementService = announcementService;
-   }
-
-   private Announcement announcement = new Announcement();
-
-   private List<Announcement> announcementsList;
-
-   @PostConstruct
+   @Override
    public void init()
    {
-      announcementsList = announcementService.search();
+      entity = new Announcement();
+      entities = announcementService.search();
    }
 
-   public Announcement getAnnouncement()
+   @Override
+   public void onLoad()
    {
-      return announcement;
-   }
-
-   public void setAnnouncement(Announcement announcement)
-   {
-      this.announcement = announcement;
-   }
-
-   public List<Announcement> getAnnouncementsList()
-   {
-      return announcementsList;
-   }
-
-   public void setAnnouncementsList(List<Announcement> announcementsList)
-   {
-      this.announcementsList = announcementsList;
+      init();
    }
 
 }
