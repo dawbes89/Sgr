cfef037c87b09d2771646dc6b51acc7b5d08b46f
leonzio <leonzio@localhost>
2016-02-27 20:53:22 +0100

    #37 poprawki

--- sgr-frontend/src/main/java/sgr/app/frontend/validators/DateValidator.java
+++ sgr-frontend/src/main/java/sgr/app/frontend/validators/DateValidator.java
@@ -3,24 +3,28 @@ package sgr.app.frontend.validators;
 import java.util.Date;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
 
 public class DateValidator extends AbstractValidator<Date>
 {
-   protected DateValidationMode mode;
 
-   public DateValidator(DateValidationMode mode)
+   private static DateValidationMode defaultMode = DateValidationMode.BEFORE;
+
+   public DateValidator()
    {
-      super(FacesMessage.SEVERITY_ERROR, mode.getLabel());
-      this.mode = mode;
+      super(FacesMessage.SEVERITY_ERROR, defaultMode.getLabel());
    }
 
    @Override
-   protected boolean isValidValue(Date value)
+   protected boolean isValidValue(Date value, final UIComponent component)
    {
-      if (value == null)
+      final String findMode = (String) component.getAttributes().get("validationMode");
+      DateValidationMode mode = defaultMode;
+      if (findMode != null && DateValidationMode.valueOf(findMode) != null)
       {
-         return true;
+         mode = DateValidationMode.valueOf(findMode);
       }
+      setErrorMessage(mode.getLabel(), new Object[] { component.getAttributes().get("label") });
       return mode.isValid(new Date(), value);
    }
 
