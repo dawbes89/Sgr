7bdbe14d947d356cc4ae8a1ca2c2fa695078f83d
dawbes89 <dawbes89@gmail.com>
2016-03-01 21:41:32 +0100

    #60 Stworzenie panelu uczniow dla wychowawcy

---
+++ sgr-app-webapp/src/main/java/sgr/app/webapp/teachingStuff/PreceptorStudentPanel.java
@@ -0,0 +1,140 @@
+package sgr.app.webapp.teachingStuff;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import sgr.app.api.assessment.Assessment;
+import sgr.app.api.assessment.AssessmentQuery;
+import sgr.app.api.assessment.AssessmentService;
+import sgr.app.api.authentication.AuthenticationService;
+import sgr.app.api.classgroup.ClassGroup;
+import sgr.app.api.classgroup.ClassGroupService;
+import sgr.app.api.comment.Comment;
+import sgr.app.api.comment.CommentService;
+import sgr.app.api.student.Student;
+import sgr.app.api.student.StudentQuery;
+import sgr.app.api.student.StudentService;
+import sgr.app.api.teachingStuff.SchoolSubject;
+import sgr.app.api.teachingStuff.TeachingStuff;
+import sgr.app.frontend.panels.AbstractPanel;
+
+/**
+ * @author dawbes89
+ */
+@Controller
+public class PreceptorStudentPanel extends AbstractPanel<Student>
+{
+
+   private static final long serialVersionUID = 8358711395309154466L;
+
+   @Autowired
+   private StudentService studentService;
+
+   @Autowired
+   private ClassGroupService classGroupService;
+
+   @Autowired
+   private AuthenticationService authenticationService;
+
+   @Autowired
+   private CommentService commentService;
+
+   @Autowired
+   private AssessmentService assessmentService;
+
+   private TeachingStuff currentLoggedTeacher;
+
+   private ClassGroup classGroup;
+
+   private SchoolSubject schoolSubject;
+
+   @Override
+   public void init()
+   {
+      entity = new Student();
+      entities = new ArrayList<>();
+   }
+
+   @Override
+   public void onLoad()
+   {
+      currentLoggedTeacher = authenticationService.getCurrentUser();
+      classGroup = currentLoggedTeacher.getPreceptorClass();
+      searchStudents();
+   }
+
+   public List<Comment> getComments()
+   {
+      return commentService.findByStudentId(entity.getId());
+   }
+
+   public void searchStudents()
+   {
+      if (classGroup == null || classGroup.getId() == null)
+      {
+         entities = new ArrayList<>();
+      }
+      else
+      {
+         final StudentQuery query = StudentQuery.withClassGroupId(classGroup.getId());
+         entities = studentService.search(query);
+      }
+   }
+
+   public List<Assessment> getAssessments()
+   {
+      if (currentLoggedTeacher == null || entity.getId() == null)
+      {
+         return new ArrayList<>();
+      }
+      return assessmentService.search(createQuery());
+   }
+
+   public String getAverageAssessments()
+   {
+      double average = 0;
+      if (currentLoggedTeacher != null && entity != null)
+      {
+         average = assessmentService.getAverageAssesment(createQuery());
+      }
+      return String.format("%1$,.2f", average);
+   }
+
+   private AssessmentQuery createQuery()
+   {
+      final AssessmentQuery query = new AssessmentQuery();
+      if (schoolSubject != null)
+      {
+         query.setSchoolSubject(schoolSubject);
+      }
+      if (entity != null && entity.getId() != null)
+      {
+         query.setStudentId(entity.getId());
+      }
+      return query;
+   }
+
+   public ClassGroup getClassGroup()
+   {
+      return classGroup;
+   }
+
+   public void setClassGroup(ClassGroup classGroup)
+   {
+      this.classGroup = classGroup;
+   }
+
+   public SchoolSubject getSchoolSubject()
+   {
+      return schoolSubject;
+   }
+
+   public void setSchoolSubject(SchoolSubject schoolSubject)
+   {
+      this.schoolSubject = schoolSubject;
+   }
+
+}
