313d07a0ff78ed716206b95b1bf4e765bf03f61d
dawbes89 <dawbes89@localhost>
2016-03-02 22:24:46 +0100

    #72 Dodanie kolumny z ocenami w panelu ocen nauczyciela

--- sgr-frontend/src/main/java/sgr/app/frontend/converters/SchoolSubjectTranslationConverter.java
+++ sgr-frontend/src/main/java/sgr/app/frontend/converters/SchoolSubjectTranslationConverter.java
@@ -1,5 +1,7 @@
 package sgr.app.frontend.converters;
 
+import javax.faces.component.UIComponent;
+
 import sgr.app.api.teachingStuff.SchoolSubject;
 
 /**
@@ -17,7 +19,7 @@ public class SchoolSubjectTranslationConverter extends AbstractTranslationConver
    }
 
    @Override
-   protected String convertToString(Object object)
+   protected String convertToString(Object object, UIComponent component)
    {
       final SchoolSubject schoolSubject = (SchoolSubject) object;
       final String translate = translationService.translate(schoolSubject.getLabel());
